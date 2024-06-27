57a6e050a434fd6dda75a345bdf8c91ca8dd4097
fami-gb <fami.gb0825@gmail.com>
2024-06-28 01:43:57 +0900

    feat:ページネーションの実装(未完成)

--- client/script.js
+++ client/script.js
@@ -1,5 +1,5 @@
-const fetchAndDisplayPosts = async () => {
-    const response = await fetch("http://localhost:8000/api/posts?page=1");
+const fetchAndDisplayPosts = async (page=1) => {
+    const response = await fetch("http://localhost:8000/api/posts?page=" + String(page));
     const postList = await response.json();
     const postListElement = document.getElementById("post-list");
     postListElement.innerHTML = "";
@@ -18,4 +18,14 @@ const fetchAndDisplayPosts = async () => {
         postListElement.appendChild(postElement);
     });
 };
-document.addEventListener("DOMContentLoaded", fetchAndDisplayPosts);
+// 次へ、前へのボタンで(1,2,3) => (2,3,4)のようにしてページネーションを実現する
+// 今は取り敢えず3ページ分だけ実装しておく
+var btn = new Array();
+for (let i = 1; i < 4; i++){
+    btn[i] = document.getElementById("btn" + String(i));
+    btn[i].addEventListener("click", () => {
+        fetchAndDisplayPosts(i);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", fetchAndDisplayPosts());
